00ad1cefff618593536f0d65281ebcc669742841
Stefan Schank <sschank@gmx.de>
2018-01-04 23:31:40 +0100

    Some tests and first steps

--- hello.go
+++ hello.go
@@ -1,19 +1,43 @@
 package main
 
+//import fmt package
 import "fmt"
 
+//declare some variables
 var a int64
+var y string
 
+// func z has no parameters
 func z() {
-	println(a)
+	y = ` hello "some text" printed out`
+	println(a + 1)
+	fmt.Println(y)
 }
 
-func main() {
-	c := 0
+//declare a new variable
+var c int
 
-	c, _ = fmt.Print("Hello World")
-	a = 10
+//create your own datatype with base type int
+type mytype int
+
+//declare a new variable based on you own datatype
+var m mytype
+
+//main function, here starts the program
+func main() {
+	//c := 42
+
+	m := 43
+	fmt.Println(m)
+	c, _ = fmt.Println("Hello World")
+	a = 12
 	fmt.Println(a, c)
 	b := "Hello new world"
-	fmt.Println(b)
+	s := fmt.Sprintf("%T\n", b)
+	// fmt.Printf("%T\n", b)
+	// fmt.Printf("%T\n", b)
+	fmt.Printf("%T", s)
+	//call function z()
+	z()
+	z()
 }
