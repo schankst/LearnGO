d3858370efb0f1390832aa39d6fc16d782a962a9
Stefan Schank <sschank@macpro.schank.it>
2018-02-03 22:15:11 +0100

    .

--- exercises/unit4/exc1-4.go
+++ exercises/unit4/exc1-4.go
@@ -7,29 +7,33 @@ import (
 func main() {
 
 	var x [5]int
-	//var y []uint
+	fmt.Printf("Array x %v\n", x)
 
 	y := []int{6, 7, 8, 2, 1, 3}
+	fmt.Printf("Slize y %v\n", y)
 
-	fmt.Println(x)
-	fmt.Println(y)
+	// fmt.Println(x)
+	// fmt.Println(y)
 
-	fmt.Println(len(x))
-	fmt.Println(cap(y))
+	// fmt.Println(len(x))
+	// fmt.Println(cap(y))
 
-	for i, v := range y {
-		fmt.Println(i, v)
-	}
+	// for i, v := range y {
+	// 	fmt.Println(i, v)
+	// }
 
-	for i := 0; i < len(y); i++ {
-		fmt.Println(i, y[i])
-	}
+	// for i := 0; i < len(y); i++ {
+	// 	fmt.Println(i, y[i])
+	// }
 
 	y = append(y, 67, 78, 73, 13, 67, 78)
-	
-	z:= y
-	
-	z append(z, y...)
+	fmt.Printf("Slize y %v\n", y)
+
+	z := y
+
+	z = append(z, y...)
+	fmt.Printf("Slize z %v\n", z)
+
 	fmt.Println(z[:3])
 	fmt.Println(z[4:])
 
